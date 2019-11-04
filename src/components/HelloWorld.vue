c3ffeddda3de57d53978810a39a074724a0aee4f
lcdada <15810227932@163.com>
2019-11-04 18:07:49 +0800

    vant

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="hello">
-    <p>{{msg }}</p>
+    <p class="textw">{{msg }}</p>
     <div class="rem">
       <div class="item1"></div>
       <div class="item2"></div>
       <div class="item3"></div>
     </div>
+     <div>
+           <van-loading type="spinner" color="#ea2000" />
+      </div>
+
   </div>
 </template>
 
 <script>
+    import { Loading } from 'vant'
     export default {
         name: 'HelloWorld',
         data () {
             return {
                 msg: 'Welcome to Your Vue.js App'
             }
+        },
+        components: {
+            [Loading.name]: Loading
         }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style  lang="stylus" scoped>
+  .textw
+    font-size 20px
   .rem{
     width: 100%;
     height: 100px;
@@ -33,16 +43,17 @@
     align-items center
 
   }
+
   .item1
-    width 50px
-    height 50px
+    width 10px
+    height 10px
     background: #333;
   .item2
-    width 50px
-    height 50px
+    width 10px
+    height 10px
     background #666
   .item3
-    width 50px
-    height 50px
+    width 10px
+    height 10px
     background #999
 </style>
